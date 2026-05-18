4d1f9805824d178aca2103749ad2949cd5b99108
Charlie <50966953+CharlieSRL@users.noreply.github.com>
2026-05-18 16:36:22 -0600

    Adding dummy script
    testing purposes only.

---
+++ TruClient/truclient_gitagent_test1/TruClient_gitagent_test1/Action.c
@@ -0,0 +1,13 @@
+//   *****************************************************************************************************************************************
+//   ****   PLEASE NOTE: This is a READ-ONLY representation of the actual script. For editing please press the "Develop Script" button.   ****
+//   *****************************************************************************************************************************************
+
+Action()
+{
+	/* Just to test the git agent */
+	/* Making change number 2 */
+	/* Change number 3 */
+	/* Change number 4 */
+
+	return 0;
+}
